a8298d0bafa2473368fba077f2a6080f2ea6ae8f
malinduruwantha <malinduruwantha@gmail.com>
2023-11-08 17:09:48 +0530

    add custom data source for user authentication

--- src/main/java/com/occw/occwinternetbanking/config/ProjectSecurityConfig.java
+++ src/main/java/com/occw/occwinternetbanking/config/ProjectSecurityConfig.java
@@ -2,31 +2,76 @@ package com.occw.occwinternetbanking.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
-
 
 @Configuration
 @EnableWebSecurity
 public class ProjectSecurityConfig {
 
 	@Bean
-	SecurityFilterChain filterChain(HttpSecurity http ) throws Exception {		
-		http.authorizeHttpRequests(auth ->
-         auth
-             .requestMatchers(AntPathRequestMatcher.antMatcher("/occwp/*")).permitAll()
-             .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.GET,"/myAccount")).authenticated()
-             .anyRequest().authenticated());
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth.requestMatchers(AntPathRequestMatcher.antMatcher("/occwp/*"))
+				.permitAll().requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/myAccount"))
+				.authenticated().anyRequest().authenticated());
 		http.formLogin(withDefaults());
 		http.httpBasic(withDefaults());
 		return http.build();
 	}
+
+	/*
+	 * @Bean public InMemoryUserDetailsManager userDetailsService() {
+	 * 
+	 * UserDetails admin = User.withDefaultPasswordEncoder() .username("admin")
+	 * .password("12345") .authorities("admin") .build(); UserDetails user =
+	 * User.withDefaultPasswordEncoder() .username("user") .password("1234")
+	 * .authorities("read") .build(); return new
+	 * InMemoryUserDetailsManager(admin,user);
+	 * 
+	 * 
+	 * UserDetails admin = User.withUsername("Admin") .password("1234")
+	 * .authorities("admin") .build(); UserDetails user = User.withUsername("user")
+	 * .password("12345") .authorities("read") .build();
+	 * 
+	 * return new InMemoryUserDetailsManager(admin,user);
+	 * 
+	 * }
+	 */
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
+	@Bean
+	UserDetailsManager users(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
+	}
+
+
+	  @Bean
+	    @ConfigurationProperties("spring.datasource")
+	    public DataSource dataSource() {
+	        return DataSourceBuilder.create().build();
+	    }
+
+
+	
 }
---
+++ src/main/resources/sql/scripts.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users (
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(500) NOT NULL,
+    enabled BOOLEAN NOT NULL,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE authorities (
+    username VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users(username)
+);
+
+CREATE UNIQUE INDEX ix_auth_username ON authorities (username, authority);
